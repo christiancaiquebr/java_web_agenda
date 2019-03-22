1f96de0542330eefe0605f998707641fcb63cf65
christian <christian.nascimento3@etec.sp.gov.br>
2019-03-21 22:18:16 -0300

    criado a classe pessoaDAO

--- src/com/agenda/service/CadastraUsuarioService.java
+++ src/com/agenda/service/CadastraUsuarioService.java
@@ -1,16 +1,15 @@
 package com.agenda.service;
 
+import com.agenda.dao.PessoaDAO;
 import com.agenda.model.Pessoa;
-import com.oracle.xmlns.internal.webservices.jaxws_databinding.SoapBindingParameterStyle;
+
 
 public class CadastraUsuarioService {
 
 	public void cadastra(Pessoa pessoa) {
-		// TODO Auto-generated method stub
-		System.out.println(pessoa.getNome());
-		System.out.println(pessoa.getTelephone());
-		System.out.println(pessoa.getEmail());
-		System.out.println(pessoa.getEndereco());
+	
+		PessoaDAO canal = new PessoaDAO();
 		
+		canal.salvar(pessoa);
 	} 
 }
