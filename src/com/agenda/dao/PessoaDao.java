7ec049504b67c38fa1e69415d7f8cd808b5e2951
christian <christian.nascimento3@etec.sp.gov.br>
2019-04-11 19:53:25 -0300

    Incluimos umas conexões na pessoaDAO

--- src/com/agenda/dao/PessoaDao.java
+++ src/com/agenda/dao/PessoaDao.java
@@ -1,6 +1,10 @@
 package com.agenda.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import com.agenda.model.Pessoa;
+import com.agenda.util.ConnectionFactory;
 import com.mysql.jdbc.Connection;
 
 public class PessoaDao {
@@ -9,6 +13,23 @@ public class PessoaDao {
 	public void cadastra(Pessoa pessoa) {
 		
 		String SQL = "insert into pessoas (nome, email, endereco, telefone) values (?,?,?,?)";
+		
+		try {
+			
+			this.connection = new ConnectionFactory().getConnection();
+			PreparedStatement stmt = this.connection.prepareStatement(SQL);
+			
+			stmt.setString(1, pessoa.getNome());
+			stmt.setString(2, pessoa.getEmail());
+			stmt.setString(3, pessoa.getEndereco());
+			stmt.setString(4, pessoa.getTelephone());
+			
+			stmt.execute();
+			stmt.close();
+			
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 		
 	}
 
